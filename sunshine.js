dba9e2832f0d660aea9d07823d3a334d494e2e59
Sarah Kurisu <sarah.kurisu@gmail.com>
2015-01-15 19:40:24 -1000

    creates reverseString, spliceString and speakPigLatin functions

--- sunshine.js
+++ sunshine.js
@@ -5,28 +5,49 @@
   Bonus: Use RegEx to create the array
 */
 function reverseString(str) {
+  var something = str.split("").reverse().join("");
+  return something;
 
 }
 
 /**
-  Write a function that takes two parameters as input in the order
-  of string, character. With the first string parameter use Array
-  functionality remove the last character and put it at the beginning
-  of the array. Then insert the character passed in the second
-  parameter into the second to last position of the array.
+  Write a function that takes two parameters as input in the order of string, character. 
+
+  With the first string parameter use Array functionality remove the last character and put it at the beginning
+  of the array. 
+
+  Then insert the character passed in the second parameter into the second to last position of the array.
+  
   Then convert the array to a string and return it.
 */
 function spliceString(str, char) {
+  var strArray = str.split("");
+  var lastChar = strArray.pop();
+  strArray.unshift(lastChar);
 
+  strArray.splice(-1, 0, char);
+
+
+  return strArray.join("");
 }
 
 /**
   Using the Array map() method write a function that takes an array
-  of strings as input and returns a duplicate array with all strings
-  in pig latin form. (Ex. "metal" => "etalmay".)
+  of strings as input and 
+
+  returns a duplicate array with all strings
+  in pig latin form. 
+
+  (Ex. "metal" => "etalmay".)
   Note: For now, do not worry about words like "chicken".
   Instead of "ickenchay", it should just become "hickencay".
 */
 function speakPigLatin(strArray) {
+  return strArray.map(function (word) {
+    var wordArray = word.split("");
+    var firstChar = wordArray.shift();
+    wordArray.push(firstChar,"ay");
+    return wordArray.join("");
+  });
 
 }
